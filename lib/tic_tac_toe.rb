27164d177451e2b30ba0e76a799dd92984ad9e7c
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-27 09:00:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,6 +35,11 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+### tests
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 ####checks
 
 def won?(board)
